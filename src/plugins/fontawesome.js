cf5dd8d78cfabfe1c6beea48cf52ffe5f4a246ea
Pedro León <pedro@wintercr.com>
2023-01-25 19:27:44 -0500

    Added mobile view
    There are some memory leaks detected.

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,10 +1,12 @@
 import Vue from "vue"
 import { library, config } from "@fortawesome/fontawesome-svg-core"
-import { faHome, faAnglesLeft } from "@fortawesome/free-solid-svg-icons"
+import { faHome, faAnglesLeft, faBars, faScissors } from "@fortawesome/free-solid-svg-icons"
+import { faHeart } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import "@fortawesome/fontawesome-svg-core/styles.css"
 
+
 config.autoAddCss = false;
-library.add(faHome, faAnglesLeft)
+library.add(faHome, faAnglesLeft, faHeart, faBars, faScissors)
 
 Vue.component("font-awesome-icon", FontAwesomeIcon)
