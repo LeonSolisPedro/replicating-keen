f0b12a44fe59990e3f73bcbe764a433a769c3609
Pedro León <pedro@wintercr.com>
2023-02-04 16:07:35 -0500

    Bug fixes for redirect to dashboard

--- src/plugins/vue-router/redirects.js
+++ src/plugins/vue-router/redirects.js
@@ -1,8 +1,8 @@
 const redirects = (to, from, next) => {
 
   //We are redirecting to corresponding dashboards
-  const superarray1 = to.matched.flatMap(record => Object.values(record.components))
-  const redirectdashboards = typeof superarray1[superarray1.length - 1].RedirectToDashboards !== "undefined"
+  const lastComponents1 = Object.values(to.matched[to.matched?.length - 1]?.components ?? {});
+  const redirectdashboards = lastComponents1.some(x => x.RedirectToDashboards)
   if (redirectdashboards) {
     const coolurl = to.path.replace(/\/$/, "");
 
@@ -16,8 +16,8 @@ const redirects = (to, from, next) => {
 
   
   //We are redirecting to index (if possible)
-  const superarray2 = to.matched.flatMap(record => Object.values(record.components))
-  const redirectindex = typeof superarray2[superarray2.length - 1].RedirectToIndex !== "undefined"
+  const lastComponents2 = Object.values(to.matched[to.matched?.length - 1]?.components ?? {});
+  const redirectindex = lastComponents2.some(x => x.RedirectToIndex)
   if (redirectindex) {
     const coolurl = to.path.replace(/\/$/, "");
     next({ path: `${coolurl}/index`, replace: true })
