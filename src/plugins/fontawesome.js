c7e1c4bb2a6ebef7d7e8e6f5d62dd19a36834ac8
Pedro León <pedro@wintercr.com>
2023-02-06 18:05:20 -0500

    Finishing with the top toolbar

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,12 +1,12 @@
 import Vue from "vue"
 import { library, config } from "@fortawesome/fontawesome-svg-core"
-import { faHome, faAnglesLeft, faBars, faScissors } from "@fortawesome/free-solid-svg-icons"
+import { faHome, faAnglesLeft, faBars, faScissors, faPlus } from "@fortawesome/free-solid-svg-icons"
 import { faHeart } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import "@fortawesome/fontawesome-svg-core/styles.css"
 
 
 config.autoAddCss = false;
-library.add(faHome, faAnglesLeft, faHeart, faBars, faScissors)
+library.add(faHome, faAnglesLeft, faHeart, faBars, faScissors, faPlus)
 
 Vue.component("font-awesome-icon", FontAwesomeIcon)
