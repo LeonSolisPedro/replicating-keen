4a873ded639cc174b2519718f72fb6a93fe83e1e
Pedro León <pedro@wintercr.com>
2023-03-13 23:04:19 -0500

    Bug fixes

--- src/views/Shared/Error/Unauthorized.vue
+++ src/views/Shared/Error/Unauthorized.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <h1>You don't belong here...</h1>
-  </div>
+	<div class="card">
+		<div class="card-body text-center d-flex flex-column flex-center py-15 px-10 py-lg-15 px-lg-15"
+			style="min-height: 67vh">
+			<h1 class="fw-bolder fs-2hx text-gray-900 mb-4">Unauthorized</h1>
+			<div class="fw-semibold fs-6 text-gray-500 mb-9">You don't belong here...</div>
+			<div class="mb-9">
+				<img src="@/assets/media/auth/401.png" class="theme-light-show" style="width:80%; max-width: 320px" />
+				<img src="@/assets/media/auth/401-dark.png" class="theme-dark-show" style="width:80%; max-width: 320px" />
+			</div>
+			<div class="mb-0">
+				<a href="../../demo1/dist/index.html" class="btn btn-sm btn-primary">Return Home</a>
+			</div>
+		</div>
+	</div>
 </template>
